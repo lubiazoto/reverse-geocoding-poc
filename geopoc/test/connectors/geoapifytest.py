46f05e2c75ec913f207cbdb0fe57806005429d79
Luciana Biazoto <luciana.biazoto@loft.com.br>
2022-04-07 13:44:38 -0300

    Novo teste realizado, resultados semelhantes, pequenos ajustes

--- geopoc/test/connectors/geoapifytest.py
+++ geopoc/test/connectors/geoapifytest.py
@@ -16,5 +16,15 @@ class GeoApiyTest(unittest.TestCase):
         self.assertIn(self.RESULT2, result_list)
 
 
+    def test_call_and_get_result(self):
+        geoapify = GeoApify()
+        result_list = geoapify.call_and_get_result("-23.5459078", "-46.5638089")
+        self.assertGreater(len(result_list), 0)  # add assertion here
+        self.assertIn(self.RESULT1, result_list)
+        self.assertIn(self.RESULT2, result_list)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
--- geopoc/test/connectors/locationiqtest.py
+++ geopoc/test/connectors/locationiqtest.py
@@ -4,16 +4,18 @@ from geopoc.domain.address import Address
 
 
 class LocationIQTest(unittest.TestCase):
-    RESULT1 = Address('Rua Emílio Mallet', '1015')
+    RESULT1 = Address('Avenida José Luiz Ferraz', '250')
 
     #  RESULT2 = Address('Rua Apucarana', '850')
 
     def test_call_and_get_result(self):
         locationiq = LocationIQ()
-        result_list = locationiq.call_and_get_result("-23.5459078", "-46.5638089")
+        result_list = locationiq.call_and_get_result("-23.020642", "-43.486812")
+        print(result_list[0])
         self.assertGreater(len(result_list), 0)  # add assertion here
         self.assertIn(self.RESULT1, result_list)
-    #    self.assertIn(self.RESULT2, result_list)
+
+
 
 
 if __name__ == '__main__':
